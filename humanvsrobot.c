13d9b0cd262847ccffc8e1ac7247722bb283675c
Igor Peixoto <igorpeixoto_@outlook.com.br>
2024-08-02 07:12:50 -0300

    Inclusão da leitura da matriz

--- humanvsrobot.c
+++ humanvsrobot.c
@@ -2,8 +2,16 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
+#include <locale.h>
 
 int main(){
+    setlocale(LC_ALL, "Portuguese");
+    FILE *arq;
+
+    arq=fopen("matriz.txt","r");
+    if(!(arq)){
+        printf("Erro, não foi possivel abrir o arquivo.");
+    }
     
     return 0;
 }
