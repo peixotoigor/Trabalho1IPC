08145ec90391e51d6ffcff6e1234b0aaa47d27ae
Igor Peixoto <igorpeixoto_@outlook.com.br>
2024-08-04 17:26:32 -0300

    Caminho implementado

--- humanvsrobot.c
+++ humanvsrobot.c
@@ -3,37 +3,94 @@
 #include <string.h>
 #include <ctype.h>
 #include <locale.h>
+// Função para imprimir o matriz
+void imprimir_matriz(char **matriz, int linhas, int colunas) {
+    for (int i = 0; i < linhas; i++) {
+        for (int j = 0; j < colunas; j++) {
+            printf("%c ", matriz[i][j]);
+        }
+        printf("\n");
+    }
+}
 
+// Função recursiva para encontrar o caminho no labirinto
+int encontrar_caminho(char **matriz, int linhas, int colunas, int x, int y) {
+    // Verifica se a posição atual é a zona de evacuação
+    if (matriz[x][y] == 'Z') {
+        return 1;  // Caminho encontrado
+    }
+
+    // Marca a posição atual como parte do caminho
+    matriz[x][y] = '*';
+
+    // Tentativas de movimento: cima, baixo, esquerda, direita
+    if (x > 0 && (matriz[x - 1][y] == ' ' || matriz[x - 1][y] == 'Z')) { // cima
+        if (encontrar_caminho(matriz, linhas, colunas, x - 1, y)) {
+            return 1;
+        }
+    }
+
+    if (x < linhas - 1 && (matriz[x + 1][y] == ' ' || matriz[x + 1][y] == 'Z')) { // baixo
+        if (encontrar_caminho(matriz, linhas, colunas, x + 1, y)) {
+            return 1;
+        }
+    }
+
+    if (y > 0 && (matriz[x][y - 1] == ' ' || matriz[x][y - 1] == 'Z')) { // esquerda
+        if (encontrar_caminho(matriz, linhas, colunas, x, y - 1)) {
+            return 1;
+        }
+    }
+
+    if (y < colunas - 1 && (matriz[x][y + 1] == ' ' || matriz[x][y + 1] == 'Z')) { // direita
+        if (encontrar_caminho(matriz, linhas, colunas, x, y + 1)) {
+            return 1;
+        }
+    }
+
+    // Desmarca a posição atual se nenhum caminho for encontrado
+    matriz[x][y] = ' ';
+    return 0;
+}
 
 int fuga_humana(char **matriz, int linhas, int colunas){
     // Encontrando as coordenadas de H e Z
-    int coordH1 = -1, coordH2 = -1; // Coordenadas de 10
-    int coordZ1 = -1, coordZ2 = -1; // Coordenadas de 20
+    int coordxH = -1, coordyH = -1; // Coordenadas de 10
+    int coordxZ = -1, coordyZ = -1; // Coordenadas de 20
     for (int i = 0; i < linhas; i++) {
         for (int j = 0; j < colunas; j++) {
             if (matriz[i][j] =='H') {
-                coordH1 = i;
-                coordH2 = j;
+                coordxH = i;
+                coordyH = j;
             }
             if (matriz[i][j] == 'Z') {
-                coordZ1 = i;
-                coordZ2 = j;
+                coordxZ = i;
+                coordyZ = j;
             }
         }
     }
 
 // Verificando se as coordenadas foram encontradas
-    if (coordH1 == -1 || coordH2 == -1 || coordZ1 == -1 || coordZ2 == -1) {
+    if (coordxH == -1 || coordyH == -1 || coordxZ == -1 || coordyZ == -1) {
         printf("Coordenadas de H ou Z não encontradas.\n");
         return -1;
     }
 
     // Imprimindo as coordenadas encontradas
-    printf("Coordenadas de H: (%d, %d)\n", coordH1, coordH2);
-    printf("Coordenadas de Z: (%d, %d)\n", coordZ1, coordZ2);
+    printf("Coordenadas de H: (%d, %d)\n", coordxH, coordyH);
+    printf("Coordenadas de Z: (%d, %d)\n", coordxZ, coordyZ);
 
-    return 0; // Retorno de sucesso
+    // Inicia a busca a partir da posição inicial do humano
+    if (encontrar_caminho(matriz, linhas, colunas, coordxH, coordyH)) {
+        printf("Caminho encontrado:\n");
+        imprimir_matriz(matriz, linhas, colunas);
+        return 1;
+    } else {
+        printf("Caminho não encontrado\n");
+        return 0;
+    }
 }
+
 
 int main(){
     setlocale(LC_ALL, "Portuguese");
@@ -85,10 +142,11 @@ int main(){
         }
         printf("\n");
     }
-
-    // Chamando a função fuga_humana
-    int resultado = fuga_humana(matriz, linhas, colunas);
-    printf("Resultado da fuga_humana: %d\n", resultado);
+    
+    // Tenta encontrar o caminho de fuga
+    if (fuga_humana(matriz, linhas, colunas)==1) {
+        printf("Caminho não encontrado\n");
+    }
 
     // Liberando a memória alocada
     for (i = 0; i < linhas; i++) {
